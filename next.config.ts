16de83b2c734e392e62507ee3ffbd70c82e994f1
shreyas <shreyas.sanghvi11@gmail.com>
2025-11-02 10:41:42 -0500

    Config: Next config to deliver static files

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig = {
     turbopack: {
         root: path.resolve(__dirname),
     },
-    basePath: '',       // no basePath since repo is username.github.io
-    assetPrefix: '',    // no asset prefix needed
 };
 
 module.exports = nextConfig;
